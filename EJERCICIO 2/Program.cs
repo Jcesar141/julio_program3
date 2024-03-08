21cf38264c6f5688e9414a164b13e7679b84fc33
Jcesar141 <julico1848@gmail.com>
2024-03-07 21:56:34 -0400

    Segunda Laboratorio

--- EJERCICIO 2/Program.cs
+++ EJERCICIO 2/Program.cs
@@ -4,27 +4,18 @@ class Program
 {
     static void Main(string[] args)
     {
-        // Solicitar al usuario que ingrese los tres valores
-        Console.WriteLine("Ingrese el primer valor:");
-        double valor1 = Convert.ToDouble(Console.ReadLine());
+        rotar();
 
-        Console.WriteLine("Ingrese el segundo valor:");
-        double valor2 = Convert.ToDouble(Console.ReadLine());
+        Console.ReadLine();
+    }
 
-        Console.WriteLine("Ingrese el tercer valor:");
-        double valor3 = Convert.ToDouble(Console.ReadLine());
-
-        // Calcular el promedio
-        double promedio = (valor1 + valor2 + valor3) / 3;
-
-        // Verificar si el promedio es mayor que 50
-        if (promedio > 50)
-        {
-            Console.WriteLine($"El promedio es {promedio}, que es mayor que 50 puntos.");
-        }
-        else
-        {
-            Console.WriteLine($"El promedio es {promedio}, que no es mayor que 50 puntos.");
-        }
+    static void rotar()
+    {
+        string numero = Console.ReadLine();
+        char primero = numero[0];
+        char segundo = numero[1];
+        string resto = numero.Substring(2, numero.length - 2);
+        string resultado = resto + primero + segundo;
+        Console.WriteLine(resultado);
     }
 }
