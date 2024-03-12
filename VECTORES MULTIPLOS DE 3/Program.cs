bab64b49ad66cc1662835fc5bebde5608726dd9a
Jcesar141 <julico1848@gmail.com>
2024-03-11 20:30:51 -0400

    Vectores Multiplos de 3

---
+++ VECTORES MULTIPLOS DE 3/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Definir un vector de cinco dígitos
+        int[] vector = { 1, 2, 3, 4, 5 };
+
+        // Mostrar los números múltiplos de 3 en el vector
+        Console.WriteLine("Los números múltiplos de 3 en el vector son:");
+        foreach (int num in vector)
+        {
+            if (num % 3 == 0) // Comprobar si el número es múltiplo de 3
+            {
+                Console.WriteLine(num); // Mostrar el número múltiplo de 3
+            }
+        }
+    }
+}
