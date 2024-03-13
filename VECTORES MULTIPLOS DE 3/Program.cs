e055e9522d8cb58efe7984f766cb629411ad3a4b
Jcesar141 <150121574+Jcesar141@users.noreply.github.com>
2024-03-12 20:24:55 -0400

    EJERCICIO 2 (#1)
    * EJERCICIO 2
    
    Ejercicio 2
    
    * Segunda Laboratorio
    
    * VECTORES SUMA DE PARES E IMPARES
    
    Suma De Numeros Pares Y Tambien Impares
    
    * Vectores Multiplos de 3
    
    * Contar la Cantidad multiplos de 3 vector

---
+++ VECTORES MULTIPLOS DE 3/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Definir un vector de cinco dígitos
+        int[] vector = { 1, 2, 3, 4, 5 };
+
+        // Variable para contar cuántos números son múltiplos de 3
+        int contadorMultiplosDe3 = 0;
+
+        // Contar los números múltiplos de 3 en el vector
+        foreach (int num in vector)
+        {
+            if (num % 3 == 0) // Comprobar si el número es múltiplo de 3
+            {
+                contadorMultiplosDe3++; // Incrementar el contador de múltiplos de 3
+            }
+        }
+
+        // Mostrar el resultado
+        Console.WriteLine("La cantidad de números múltiplos de 3 en el vector es: " + contadorMultiplosDe3);
+    }
+}
